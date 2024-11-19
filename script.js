28ce78389fbfecb11ce7a2f702f7121d290933b7
Josh Philip <158793363+FlaminWyvern@users.noreply.github.com>
2024-11-19 16:54:02 -0500

    updated the styles to make it look nicer

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+const textToType = "Hello,\nI'm Josh Philip,\nAspiring Cybersecurity Engineer";
+const typedText = document.getElementById("typed-text");
+
+let charIndex = 0;
+
+function typeEffect() {
+    if (charIndex < textToType.length) {
+        if (textToType.charAt(charIndex) === "\n") {
+            typedText.innerHTML += "<br>";
+        } else {
+            typedText.innerHTML += textToType.charAt(charIndex);
+        }
+        charIndex++;
+        setTimeout(typeEffect, 100); 
+    }
+}
+
+window.onload = typeEffect; 
